87d82dd6b6c1ed2467e4e1951a437cec575fbc35
JesusPrieto18 <jlpt.1910@gmail.com>
2025-10-21 12:54:50 -0400

    Create class Karts

--- src/kart.ts
+++ src/kart.ts
@@ -2,18 +2,31 @@ import * as THREE from 'three';
 import { scene } from './scene';
 import { solidWithWire } from './utils/utils';
 
-export let kart: THREE.Group;
+export class Kart {
+  private powerUps: number = 0;
+  private activatePowerUps: boolean = false;
+  private full_kart = new THREE.Group();
 
-export function createKart(): void {
-  const height = 1;
-  const length = 1;
-  const width = 2;
+  constructor() {
+    const height = 1;
+    const length = 1;
+    const width = 2;
 
-  const geometry = new THREE.BoxGeometry(length, height, width);
-  const material_color = 0xff0000;
-  kart = solidWithWire(geometry, material_color, false);
-  kart.position.set(2, 0.5, 2);
-  kart.name = 'kart';
-  scene.add(kart);
+    const geometry = new THREE.BoxGeometry(length, height, width);
+    const material_color = 0xff0000;
+    const body = solidWithWire(geometry, material_color, false);
+    body.name = 'body';
+    
+    this.full_kart.position.set(2, 0.5, 2);
+    this.full_kart.add(body);
+    scene.add(this.full_kart);
+  }
 
+  public getFullKart(): THREE.Group {
+    return this.full_kart;
+  }
+
+  public getBody(): THREE.Mesh {
+    return this.full_kart.getObjectByName('body') as THREE.Mesh;
+  }
 }
