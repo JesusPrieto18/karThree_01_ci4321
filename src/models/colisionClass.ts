9132e34b1d8b372fa3fb4b3bf0d0c12ac3d555b8
Jesús David Cuéllar <jedave.co@gmail.com>
2025-10-24 17:24:07 -0400

    Bomb added to CollisionClass

--- src/models/colisionClass.ts
+++ src/models/colisionClass.ts
@@ -3,8 +3,9 @@ import { Shuriken } from "../shuriken";
 import { TrafficCone } from "../trafficCone";
 import { Walls } from "../walls";
 import { Kart } from "../kart";
+import { Bomb } from "../bomb";
 
-export type CollisionClassName = Kart | PowerUp | Shuriken | TrafficCone | Walls;
-export type Proyectils = Shuriken;
+export type CollisionClassName = Kart | PowerUp | Shuriken | TrafficCone | Walls | Bomb;
+export type Proyectils = Shuriken | Bomb;
 export type ReflectObjects = Kart | Shuriken;
 export type StaticObjects = Walls;
